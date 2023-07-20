b9ec4aa068a4de5f04447b71e96be382c36d8dc2
Joanna K <joannakleinrm@gmail.com>
2023-07-20 17:48:10 +0200

    navbar

--- my-app/src/layouts/RootLayout.jsx
+++ my-app/src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
+import { NavBar } from "../components/NavBar"
 
 export default function RootLayout() {
   return (
-    <div>
-      <Outlet />
+      <div>
+        <NavBar/>
+        <Outlet />
     </div>
   )
 }
